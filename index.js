e2e45257e6c573c186e80be1d758c268becbfbb0
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-18 21:59:13 +0530

    Initialized captcha variable and generate captcha on page load

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const submit = document.getElementById("_submit");
 const refresh = document.getElementById("_refresh");
 
 const char = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+let captcha = "";
+
+
+body.onload = function generateCaptcha()
