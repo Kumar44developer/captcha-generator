013686d1fb043ff7c826ca223e9d81fd6481ce0e
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-18 09:17:49 +0530

    Added refresh button reference and character set for captcha generation

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const display = document.getElementById("_status");
 const body = document.body;
 const submit = document.getElementById("_submit");
+const refresh = document.getElementById("_refresh");
+
+const char = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
